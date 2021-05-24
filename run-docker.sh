7dd1a151479f43e34bb7ad927342452a4ef1214d
d-gurskiy <d-gurskiy@debian-work.local>
2021-05-25 02:30:04 +0300

    rewrote readme, added base Dockerfile for ansible, rewrote startup script

--- run-docker.sh
+++ run-docker.sh
@@ -1,13 +1,17 @@
-#!/bin/bash -x
+#/bin/bash -x
 
 if [ "$1" = "--ansible" ] 
 then
-	echo " запуск ansible контейнера"
+	docker build -t ansible ./ansible/
+	docker run -d -it  --name ansible ansible
+	docker exec -it ansible sh
 fi
 
 if [ "$1" = "--terraform" ] 
 then
-	echo " запуск terraform контейнера"
+	docker build -t terraform ./terraform/
+	docker run -d -it --name terraform terraform
+ 	docker exec -it terraform sh
 fi
 
 if [ "$1" != "--terraform" ] && [ "$1" != "--ansible" ] 
