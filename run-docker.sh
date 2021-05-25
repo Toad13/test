6f3e56db4575b7c8ffc6d6175e9917cdb194b0b9
d-gurskiy <d-gurskiy@debian-work.local>
2021-05-25 22:15:08 +0300

    added run-docker.sh, and rewrote ansible Dokerfile

--- run-docker.sh
+++ run-docker.sh
@@ -3,18 +3,20 @@
 if [ "$1" = "--ansible" ] 
 then
 	docker build -t ansible ./ansible/
-	docker run -d -it  --name ansible ansible
-	docker exec -it ansible sh
+	docker run -d -it --rm --name ansible -v $(pwd)/keys:/root/.ssh ansible
+	docker exec -it ansible /bin/bash
 fi
 
 if [ "$1" = "--terraform" ] 
 then
 	docker build -t terraform ./terraform/
-	docker run -d -it --name terraform terraform
- 	docker exec -it terraform sh
+	docker run -d -it --rm --name terraform -v $(pwd)/keys:/root/.ssh terraform
+ 	docker exec -it terraform /bin/bash
 fi
 
 if [ "$1" != "--terraform" ] && [ "$1" != "--ansible" ] 
 then
 	echo " usage ./run-docker.sh key"
+	echo " --ansible - run ansible container"
+	echo " --terraform - run terraform container"
 fi
