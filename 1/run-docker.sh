a5ac69deb92a9ef06f08e7fe461b2555faae1f3e
d-gurskiy <d-gurskiy@debian-work.local>
2021-05-26 23:53:12 +0300

    rewrote the startup script, added variables to the ansible image, dropped a couple of playbooks. Rewrote terraform Dokerfile.

--- 1/run-docker.sh
+++ 1/run-docker.sh
@@ -1,20 +1,16 @@
 #/bin/bash -x
 
-if [ "$1" = "--ansible" ] 
-then
+if [ "$1" = "--ansible" ]; then 
 	docker build -t ansible ./ansible/
 	docker run -d -it --rm --name ansible -v $(pwd)/keys:/root/.ssh -v $(pwd)/ansible/playbook:/root/playbook -w /root/ ansible
 	docker exec -it ansible /bin/bash
-fi
-
-if [ "$1" = "--terraform" ] 
-then
-	docker run -d --rm -it --name terraform --entrypoint "/usr/bin/tail" -v $(pwd)/terraform:/workspace -w /workspace -v $(pwd)/keys:/root/.ssh hashicorp/terraform:light sh tail -f /dev/null
- 	docker exec -it terraform sh
-fi
-
-if [ "$1" != "--terraform" ] && [ "$1" != "--ansible" ] 
-then
+	docker stop ansible
+elif [ "$1" = "--terraform" ]; then
+	docker build -t terraform ./terraform/
+	docker run -d -it --rm --name terraform -v $(pwd)/keys:/root/.ssh -w /root/ terraform
+	docker exec -it terraform sh
+ 	docker stop terraform
+elif [ "$1" != "--terraform" ] && [ "$1" != "--ansible" ];then
 	echo " usage ./run-docker.sh key"
 	echo " --ansible - run ansible container"
 	echo " --terraform - run terraform container"
